08d6b03641fe7ad2182914cd26c70fc6c2fe028a
Joyanta Mondal <56169323+w3codelabs@users.noreply.github.com>
2021-09-25 15:11:04 +0600

    updated cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import './Cart.css';
+
 const Cart = (props) => {
     const {cart}= props;
-    const totalReducer = (previous,product)=>previous+product.price;
+  
+    let totalReducer = (previous,product)=>previous+product.price;
     const total = cart.reduce(totalReducer,0);
     // let total =0;
     // for(const product of cart){
     //     total= total+product.price;
     // }
-    const shipping = 15;
-    const tax = (total+shipping)*10;
+  
+    let totalQuantity = 0;
+    for(const product of cart){
+        // product.quantity = !product.quantity ? 1: product.quantity;
+        if(!product.quantity){
+            product.quantity=1;
+        }
+        totalReducer = totalReducer+ product.quantity
+        totalQuantity= totalQuantity+ product.quantity;
+    }
+    const shipping = total>0 ?15:0;
+    const tax = (total+shipping)*0.10;
     const grandTotal= total+shipping+tax;
+    
     return (
         <div>
             <h2>Order Summary</h2>
-                <h5>Items Ordered: {props.cart.length}</h5><br />
+                <h5>Items Ordered: {totalQuantity}</h5><br />
                 <p>Total Price: {total.toFixed(2)}</p>
                 <p>Shipping: {shipping}</p>
                 <p>Tax: {tax.toFixed(2)}</p>
